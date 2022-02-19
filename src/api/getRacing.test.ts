058b33e5a92b31d6762fce5b3d9c95eeee31c360
Zekun Fang <>
2022-02-20 10:50:11 +1100

    remove unused code

--- src/api/getRacing.test.ts
+++
@@ -1,8 +0,0 @@
-import {getRacing} from './getRacing';
-
-describe('test get racing api', () => {
-  it('should return data', async () => {
-    var result = await getRacing();
-    console.log('result', result);
-  });
-});
--- src/interfaces/advertisedStart.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface AdvertisedStart {
-  seconds: number;
-}
