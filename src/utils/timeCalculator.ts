c4e56735daa63354154273c9111ad31cc8fc9ae3
Zekun Fang <>
2022-02-19 18:48:49 +1100

    sort games by time

--- src/utils/timeCalculator.ts
+++ src/utils/timeCalculator.ts
@@ -2,3 +2,20 @@ export const timeCalculator = (unixTime: number): number => {
   const currentTime = Math.round(new Date().getTime() / 1000);
   return unixTime - currentTime;
 };
+export const fancyTimeFormat = (duration: number) => {
+  // Hours, minutes and seconds
+  var hrs = Math.floor(duration / 3600);
+  var mins = Math.floor((duration % 3600) / 60);
+  var secs = Math.floor(duration % 60);
+
+  // Output like "1:01" or "4:03:59" or "123:03:59"
+  var ret = '';
+
+  if (hrs > 0) {
+    ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
+  }
+
+  ret += '' + mins + ':' + (secs < 10 ? '0' : '');
+  ret += '' + secs;
+  return ret;
+};
